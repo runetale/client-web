5f18fe9bdc487876673f978b847e6aacdb3ae9b5
GitHub Action <action@github.com>
2022-06-20 04:00:52 +0000

    Sync to proto commit
    Notch-Technologies/proto@d4b043e47ac27db5f1985a2a42003ee911508491

---
+++ notch/dotshake/v1/health.ts
@@ -0,0 +1,228 @@
+/* eslint-disable */
+import Long from "long";
+import { grpc } from "@improbable-eng/grpc-web";
+import * as _m0 from "protobufjs/minimal";
+import { Empty } from "../../../google/protobuf/empty";
+import { BrowserHeaders } from "browser-headers";
+
+export const protobufPackage = "protos";
+
+export interface HealthResponse {
+  message: string;
+}
+
+function createBaseHealthResponse(): HealthResponse {
+  return { message: "" };
+}
+
+export const HealthResponse = {
+  encode(
+    message: HealthResponse,
+    writer: _m0.Writer = _m0.Writer.create()
+  ): _m0.Writer {
+    if (message.message !== "") {
+      writer.uint32(10).string(message.message);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): HealthResponse {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseHealthResponse();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.message = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): HealthResponse {
+    return {
+      message: isSet(object.message) ? String(object.message) : "",
+    };
+  },
+
+  toJSON(message: HealthResponse): unknown {
+    const obj: any = {};
+    message.message !== undefined && (obj.message = message.message);
+    return obj;
+  },
+
+  fromPartial<I extends Exact<DeepPartial<HealthResponse>, I>>(
+    object: I
+  ): HealthResponse {
+    const message = createBaseHealthResponse();
+    message.message = object.message ?? "";
+    return message;
+  },
+};
+
+export interface HealthService {
+  Health(
+    request: DeepPartial<Empty>,
+    metadata?: grpc.Metadata
+  ): Promise<HealthResponse>;
+}
+
+export class HealthServiceClientImpl implements HealthService {
+  private readonly rpc: Rpc;
+
+  constructor(rpc: Rpc) {
+    this.rpc = rpc;
+    this.Health = this.Health.bind(this);
+  }
+
+  Health(
+    request: DeepPartial<Empty>,
+    metadata?: grpc.Metadata
+  ): Promise<HealthResponse> {
+    return this.rpc.unary(
+      HealthServiceHealthDesc,
+      Empty.fromPartial(request),
+      metadata
+    );
+  }
+}
+
+export const HealthServiceDesc = {
+  serviceName: "protos.HealthService",
+};
+
+export const HealthServiceHealthDesc: UnaryMethodDefinitionish = {
+  methodName: "Health",
+  service: HealthServiceDesc,
+  requestStream: false,
+  responseStream: false,
+  requestType: {
+    serializeBinary() {
+      return Empty.encode(this).finish();
+    },
+  } as any,
+  responseType: {
+    deserializeBinary(data: Uint8Array) {
+      return {
+        ...HealthResponse.decode(data),
+        toObject() {
+          return this;
+        },
+      };
+    },
+  } as any,
+};
+
+interface UnaryMethodDefinitionishR
+  extends grpc.UnaryMethodDefinition<any, any> {
+  requestStream: any;
+  responseStream: any;
+}
+
+type UnaryMethodDefinitionish = UnaryMethodDefinitionishR;
+
+interface Rpc {
+  unary<T extends UnaryMethodDefinitionish>(
+    methodDesc: T,
+    request: any,
+    metadata: grpc.Metadata | undefined
+  ): Promise<any>;
+}
+
+export class GrpcWebImpl {
+  private host: string;
+  private options: {
+    transport?: grpc.TransportFactory;
+
+    debug?: boolean;
+    metadata?: grpc.Metadata;
+  };
+
+  constructor(
+    host: string,
+    options: {
+      transport?: grpc.TransportFactory;
+
+      debug?: boolean;
+      metadata?: grpc.Metadata;
+    }
+  ) {
+    this.host = host;
+    this.options = options;
+  }
+
+  unary<T extends UnaryMethodDefinitionish>(
+    methodDesc: T,
+    _request: any,
+    metadata: grpc.Metadata | undefined
+  ): Promise<any> {
+    const request = { ..._request, ...methodDesc.requestType };
+    const maybeCombinedMetadata =
+      metadata && this.options.metadata
+        ? new BrowserHeaders({
+            ...this.options?.metadata.headersMap,
+            ...metadata?.headersMap,
+          })
+        : metadata || this.options.metadata;
+    return new Promise((resolve, reject) => {
+      grpc.unary(methodDesc, {
+        request,
+        host: this.host,
+        metadata: maybeCombinedMetadata,
+        transport: this.options.transport,
+        debug: this.options.debug,
+        onEnd: function (response) {
+          if (response.status === grpc.Code.OK) {
+            resolve(response.message);
+          } else {
+            const err = new Error(response.statusMessage) as any;
+            err.code = response.status;
+            err.metadata = response.trailers;
+            reject(err);
+          }
+        },
+      });
+    });
+  }
+}
+
+type Builtin =
+  | Date
+  | Function
+  | Uint8Array
+  | string
+  | number
+  | boolean
+  | undefined;
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type Exact<P, I extends P> = P extends Builtin
+  ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & Record<
+        Exclude<keyof I, KeysOfUnion<P>>,
+        never
+      >;
+
+if (_m0.util.Long !== Long) {
+  _m0.util.Long = Long as any;
+  _m0.configure();
+}
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined;
+}
